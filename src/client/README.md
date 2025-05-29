5014653ddf99f324dc1a4c2510bd0d35a3fb2f44
fraxken <gentilhomme.thomas@gmail.com>
2025-05-29 22:39:08 +0200

    chore: setup client package

---
+++ src/client/README.md
@@ -0,0 +1,7 @@
+<p align="center"><h1 align="center">
+  Client
+</h1>
+
+<p align="center">
+  JollyPixel Vue.js client
+</p>
---
+++ src/client/src/main.ts
@@ -0,0 +1,8 @@
+// Import Third-party Dependencies
+import { createApp } from "vue";
+
+// Import Internal Dependencies
+import "./style.css";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
