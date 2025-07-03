4fb0666a667beb9cb86e25c34df87e138b12c38a
fraxken <gentilhomme.thomas@gmail.com>
2025-07-03 15:32:57 +0200

    feat: first implementation of dfs-tree

---
+++ packages/fs-tree/src/index.ts
@@ -0,0 +1,30 @@
+// Import Internal Dependencies
+import { FSTree } from "./FSTree.class.js";
+import type { FSTreeFile } from "./types.js";
+
+interface CustomFSNode extends FSTreeFile {
+  type: "script";
+}
+
+const fsTree = await FSTree.loadFromPath<CustomFSNode>(
+  process.cwd(),
+  (dirent) => {
+    return {
+      name: dirent.name,
+      type: "script"
+    };
+  }
+);
+fsTree.on(FSTree.Event, (op) => {
+  console.log("tree updated op:", op);
+});
+
+fsTree.mvdir("/src/utils", "/src/helpers");
+fsTree.copy(
+  { parentPath: "/src", name: "types.ts" },
+  "/src/helpers"
+);
+
+console.log(
+  [...fsTree.readdir("/src", { recursive: true, absolutePath: true })]
+);
