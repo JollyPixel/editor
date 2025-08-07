b7e18ffe18708199a515bf0e61b1fca09b1cc280
Pierre Demailly <pierredemailly.pro@gmail.com>
2025-08-07 23:27:36 +0200

    feat(editors/model): add support for fbx

--- packages/editors/model/src/loaders.ts
+++ packages/editors/model/src/loaders.ts
@@ -5,16 +5,19 @@ import {
 import * as THREE from "three";
 import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
 import { MTLLoader } from "three/addons/loaders/MTLLoader.js";
+import { FBXLoader } from "three/addons/loaders/FBXLoader.js";
 
 export const modelLoader = Systems.Assets.registerLoader<THREE.Group<THREE.Object3DEventMap>>(
   {
-    extensions: [".obj"],
+    extensions: [".obj", ".fbx"],
     type: "model"
   },
   (asset, context) => {
     switch (asset.ext) {
       case ".obj":
         return objectLoader(asset, context);
+      case ".fbx":
+        return fbxLoader(asset, context);
       default:
         throw new Error(`Unsupported model type: ${asset.ext}`);
     }
@@ -67,4 +70,26 @@ function isMaterialWithMap(
     material instanceof THREE.MeshStandardMaterial ||
     material instanceof THREE.MeshPhongMaterial
   );
+}
+
+function fbxLoader(
+  asset: Systems.Asset,
+  context: Systems.AssetLoaderContext
+): Promise<THREE.Group<THREE.Object3DEventMap>> {
+  const { manager } = context;
+
+  const loader = new FBXLoader(manager)
+    .setPath(asset.path);
+
+  return new Promise((resolve, reject) => {
+    loader.load(
+      asset.basename,
+      (object) => {
+        object.name = asset.name;
+        resolve(object);
+      },
+      void 0,
+      (error) => reject(error)
+    );
+  });
 }
