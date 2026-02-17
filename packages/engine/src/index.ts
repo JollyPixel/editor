bbfd229039a3ff6b65c5ca5ae532ef68d100a317
GENTILHOMME Thomas <gentilhomme.thomas@gmail.com>
2026-02-17 04:59:26 +0100

    chore: export GlobalAudio class

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -4,6 +4,7 @@ export * from "./actor/index.ts";
 export * from "./controls/Input.class.ts";
 export * from "./audio/AudioBackground.ts";
 export * from "./audio/AudioManager.ts";
+export * from "./audio/GlobalAudio.ts";
 export * from "./controls/CombinedInput.ts";
 export * from "./ui/index.ts";
 export * from "./utils/index.ts";
